0547e7cc751e720452079ddee12662e7ad19311c
Simon Bernardin <simon.bernardin@moventes.com>
2019-06-06 22:16:03 +0200

    added bottom navigation bar

--- app/src/main/java/com/moventes/moventest/tmdb/MainActivity.kt
+++ app/src/main/java/com/moventes/moventest/tmdb/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.moventes.moventest.tmdb
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.moventes.moventest.tmdb.fragments.recentmovies.RecentMoviesFragment
 import com.moventes.moventest.tmdb.models.Movie
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), RecentMoviesFragment.OnListFragmentInteractionListener {
-
+class MainActivity : AppCompatActivity(), RecentMoviesFragment.OnListFragmentInteractionListener,
+    BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Snackbar.make(this.coordinatorLayout, item.itemId.toString(), Snackbar.LENGTH_SHORT).show()
+        return true
     }
 
     override fun onListFragmentInteraction(movie: Movie) {
